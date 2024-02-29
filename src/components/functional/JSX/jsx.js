b11ac82ef49d1e8d5ea41fbf94c30f75cdc7ec60
navyasadanapalli <navyasadanapalli@gmail.com>
2024-02-29 18:31:08 +0530

    practced

---
+++ src/components/functional/JSX/jsx.js
@@ -0,0 +1,10 @@
+import {Component} from "react";
+
+class JsxClass extends Component{
+    render(){
+        return(
+            <h2>Hello JSX</h2>
+        )
+    }
+}
+export default JsxClass
---
+++ src/components/functional/JSX/jsxfunctional.js
@@ -0,0 +1,16 @@
+ const JsxFunctional = ()=>{
+   const object ={
+    name:"navya",
+    age : 21
+   }
+   return(
+    <div>
+        <h2> 
+            good morning{object.name}
+        </h2>
+        <h3>{object.name}</h3>
+        <button onClick={()=>{}}>click</button>
+    </div>
+   )
+ } 
+ export default JsxFunctional

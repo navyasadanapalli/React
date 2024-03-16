9f4c6adde7fc953b2eb4857796333eb1362e7df2
navyasadanapalli <navyasadanapalli@gmail.com>
2024-03-16 17:17:27 +0530

    task completed

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 // import ConditionalRender from "./components/functional/exports-imports/conditional-render";
 // import Navya,{ComponentB as Kavya,ComponentC} from "./components/functional/exports-imports/exports";
 
-import { AccordionItem } from "react-bootstrap";
+import { AccordionItem, Modal } from "react-bootstrap";
 import TodoList from "./components/functional/styles";
 import AccordionComponent from "./components/functional/styles/ReactBootstrap/accordion";
 import ContentPage from "./components/functional/styles/bootstrap/content";
 import Corousel from "./components/functional/styles/ReactBootstrap/corousel";
+import Modals from "./components/functional/styles/ReactBootstrap/modals";
+import NavBar from "./components/functional/styles/ReactBootstrap/navbar";
+import Toasts from "./components/functional/styles/ReactBootstrap/toasts";
+import Tables from "./components/functional/styles/ReactBootstrap/tables";
 
 
 
@@ -25,7 +29,11 @@ function App() {
 
     {/* <AccordionComponent/> */}
 
+    <NavBar/>
     <Corousel/>
+    <Tables/>   
+    <Toasts/>
+    <Modals/>
 
     </div>
   );

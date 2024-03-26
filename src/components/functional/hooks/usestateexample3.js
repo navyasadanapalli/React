76315d921fff1fbfc00ee8d436d1e439d1c327f6
navyasadanapalli <navyasadanapalli@gmail.com>
2024-03-26 14:31:44 +0530

    task complted

--- src/components/functional/hooks/usestateexample3.js
+++ src/components/functional/hooks/usestateexample3.js
@@ -3,15 +3,35 @@ import { useState } from "react"
 
 const UseStateExample3=()=>{
     const[fruits,setfruits]=useState(["banana","apple","mango"])
+    const[newfruit,setNewFruit]=useState("")
     const addfruits=()=>{
-        const newlist=[...fruits,"new fruits"]
+        const newlist=[...fruits,newfruit]
         setfruits(newlist)
+        setNewFruit("")
+    }
+    const deletefruit=(index)=>{
+    if(fruits.length>0){
+        const newlist=[...fruits]
+                newlist.pop()
+                setfruits(newlist)
+
+
+    }
+  
     }
    
     return(
         <>
-        <h1>fruit listing</h1>
+
+          <h1>Fruit Listing</h1>
+      <input
+        type="text"
+        value={newfruit}
+        onChange={(e) => setNewFruit(e.target.value)}
+        placeholder="Enter new fruit"
+      /> 
         <button onClick={addfruits}>Add fruits</button>
+        <button onClick={deletefruit}>delete fruits</button>
         <ol>
             {
                 fruits.map((eachfruit,index)=><li key={index}>{eachfruit}</li>)
@@ -21,3 +41,5 @@ const UseStateExample3=()=>{
     )
 }
 export default UseStateExample3;
+
+
